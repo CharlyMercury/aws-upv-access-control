08aeae1b26e882d7477728b8b3ed562e9ae75e4e
Charly Mercury <carlos.mercury92@gmail.com>
2024-09-02 00:18:26 -0600

    Added homeworks and classes

--- main_producer.py
+++ main_producer.py
@@ -1,14 +1,28 @@
 import json
-from src.producer import producer_sqs
+import sys
+from src.producer import producer_student_attendance, producer_student_homework
 
 with open(file=r"parameters_sqs.json", mode="r", encoding="utf-8") as file:
     parameters = json.load(file)
 
+with open(file=r"classes/class_day_1.json", mode="r", encoding="utf-8") as file:
+    parameters_class = json.load(file)
 
-def run():
-    print("\t\t---Control de Acceso---\n\t---Metodología de la programación---\n")
-    producer_sqs(parameters)
+with open(file=r"homeworks/homework_week_1.json", mode="r", encoding="utf-8") as file:
+    parameters_homework = json.load(file)
+
+
+def run(args):
+    if args=="asistencia": 
+        print("\t\t---Control de Acceso---\n\t---Metodología de la programación---\n")
+        producer_student_attendance(parameters, parameters_class)
+    elif args == "tarea":
+        print("\t\t---Tarea Semanal---\n\t---Metodología de la programación---\n")
+        producer_student_homework(parameters, parameters_homework)
+    else:
+        print("Opción inválida")
 
 
 if __name__ == "__main__":
-    run()
+    args = sys.argv[1]
+    run(args)
--- src/producer.py
+++ src/producer.py
@@ -2,8 +2,11 @@ import time
 import boto3
 import json
 
+email_termination = "@upv.edu.mx"
+course = " Metodología de la programación "
 
-def producer_sqs(parameters):
+
+def producer_student_attendance(parameters, parameters_class):
 
     client = boto3.client("sqs",
                         region_name=parameters["aws_region"],
@@ -11,20 +14,12 @@ def producer_sqs(parameters):
                         aws_secret_access_key=parameters["aws_secret_key"])
     queue_url = parameters["queue_url"]
 
-    course = " Metodología de la programación "
-    subject = f" Asistencia al curso: {course}. "
-    day_objective = """
-    <h4> Objetivos del Día:</h4>
-    <ul> 
-        <li>Definir qué es un algoritmo.</li>
-        <li>Identificar ejemplos de algoritmos en la vida diaria.</li>
-    </ul>
-    """
-    email_termination = "@gmail.com"
+    subject = f"{parameters_class["subject"]}: {course}. "
+    day_objective = parameters_class["content"]
 
     while True:
 
-        matricula = input(" Capturar matrícula ")
+        matricula = input(" Capturar matrícula: ")
 
         if matricula == '1030034':
             break
@@ -45,3 +40,38 @@ def producer_sqs(parameters):
         if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             print(f"\tAlumno {matricula} capturado correctamente\t\n")
 
+
+def producer_student_homework(parameters, parameters_homework):
+
+    client = boto3.client("sqs",
+                        region_name=parameters["aws_region"],
+                        aws_access_key_id=parameters["aws_key_id"],
+                        aws_secret_access_key=parameters["aws_secret_key"])
+    queue_url = parameters["queue_url"]
+
+    subject = f" {parameters_homework["subject"]}: {course}. "
+    week_homework = parameters_homework["content"]
+
+    group_id = input(" Capturar id grupo (04, 11, 18): ")
+    group = f"groups/IM_20-750{group_id}.json"    
+
+    with open(file=group, mode="r", encoding="utf-8") as file:
+        parameters_group = json.load(file)
+
+    for student in parameters_group["students_id"]:
+        matricula = str(student)
+        timestamp = time.time()
+
+        response = client.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps({
+                "matricula": matricula,
+                "timestamp": timestamp,
+                "subject": subject,
+                "content": week_homework,
+                "receiver": matricula+email_termination,
+            })
+        )
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            print(f"\tAlumno {matricula}: Tarea enviada correctamente. \t\n")
