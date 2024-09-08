09f89b934200a0c177c066502dc20525aaa0eb16
Charly Mercury <carlos.mercury92@gmail.com>
2024-09-08 12:03:13 -0600

    Notifications added & files restructuration

--- main_producer.py
+++ main_producer.py
@@ -1,16 +1,18 @@
 import json
 import sys
-from src.producer import producer_student_attendance, producer_student_homework
+from src.producer import producer_student_attendance, producer_student_homework, producer_student_email_notifications
 
 with open(file=r"parameters_sqs.json", mode="r", encoding="utf-8") as file:
     parameters = json.load(file)
 
-with open(file=r"classes/class_day_1.json", mode="r", encoding="utf-8") as file:
+with open(file=r"classes/week_2/class_day_1.json", mode="r", encoding="utf-8") as file:
     parameters_class = json.load(file)
 
-with open(file=r"homeworks/homework_week_1.json", mode="r", encoding="utf-8") as file:
+with open(file=r"homeworks/week_2/homework_1.json", mode="r", encoding="utf-8") as file:
     parameters_homework = json.load(file)
 
+with open(file=r"notifications/week_2/notification_1.json", mode="r", encoding="utf-8") as file:
+    parameters_notifications = json.load(file)
 
 def run(args):
     if args=="asistencia": 
@@ -19,6 +21,9 @@ def run(args):
     elif args == "tarea":
         print("\t\t---Tarea Semanal---\n\t---Metodología de la programación---\n")
         producer_student_homework(parameters, parameters_homework)
+    elif args == "avisos":
+        print("\t\t---Envío de avisos---\n\t---Metodología de la programación---\n")
+        producer_student_email_notifications(parameters, parameters_notifications)
     else:
         print("Opción inválida")
 
@@ -26,3 +31,4 @@ def run(args):
 if __name__ == "__main__":
     args = sys.argv[1]
     run(args)
+    
--- src/producer.py
+++ src/producer.py
@@ -75,3 +75,39 @@ def producer_student_homework(parameters, parameters_homework):
 
         if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             print(f"\tAlumno {matricula}: Tarea enviada correctamente. \t\n")
+
+
+def producer_student_email_notifications(parameters, parameters_homework):
+    
+    client = boto3.client("sqs",
+                        region_name=parameters["aws_region"],
+                        aws_access_key_id=parameters["aws_key_id"],
+                        aws_secret_access_key=parameters["aws_secret_key"])
+    queue_url = parameters["queue_url"]
+
+    subject = f" {parameters_homework["subject"]}: {course}. "
+    week_homework = parameters_homework["content"]
+
+    group_id = input(" Capturar id grupo (04, 11, 18): ")
+    group = f"groups/IM_20-750{group_id}.json"    
+
+    with open(file=group, mode="r", encoding="utf-8") as file:
+        parameters_group = json.load(file)
+
+    for student in parameters_group["students_id"]:
+        matricula = str(student)
+        timestamp = time.time()
+
+        response = client.send_message(
+            QueueUrl=queue_url,
+            MessageBody=json.dumps({
+                "matricula": matricula,
+                "timestamp": timestamp,
+                "subject": subject,
+                "content": week_homework,
+                "receiver": matricula+email_termination,
+            })
+        )
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            print(f"\t Aviso enviado correctamente a {matricula}. \t\n")
